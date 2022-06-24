64621bf6412851450fb9e03bc7881573308bd08a
son <sangbae6840@gmail.com>
2022-06-24 17:56:10 +0900

    자동 로그인 & 앱아이콘

--- app/src/main/java/com/ms/daelimtime/activity/LoginActivity.kt
+++ app/src/main/java/com/ms/daelimtime/activity/LoginActivity.kt
@@ -18,32 +18,35 @@ class LoginActivity : AppCompatActivity() {
     val spinner: Spinner by lazy {
         findViewById(R.id.class_spinner)
     }
-    val userClassNum_edit : EditText by lazy{
+    val userClassNum_edit: EditText by lazy {
         findViewById(R.id.userClassNum_edit)
     }
-    val userNickName_edit : EditText by lazy{
+    val userNickName_edit: EditText by lazy {
         findViewById(R.id.userNickName_edit)
     }
 
-    lateinit var userNickname : String
-    lateinit var userClassNum : String // 학번
-    lateinit var userClass : String // 학과
+
+    lateinit var userNickname: String
+    lateinit var userClassNum: String // 학번
+    lateinit var userClass: String // 학과
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        autoLogin()
         //학과 학번 닉네임 받기
         UserInit()
 
-        var nextBtn : Button = findViewById(R.id.next_Btn)
-        nextBtn.setOnClickListener{
+
+        var nextBtn: Button = findViewById(R.id.next_Btn)
+        nextBtn.setOnClickListener {
 
             UserDataSet()
 
-            var intent : Intent = Intent(this,MainActivity::class.java)
+            var intent: Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
 
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
@@ -90,17 +93,23 @@ class LoginActivity : AppCompatActivity() {
         }
 
 
-        val kakao_Btn = findViewById<ImageButton>(R.id.kakao_login_button) // 로그인 버튼
+        val kakao_Btn = findViewById<ImageButton>(R.id.kakao_login_button)
+
+
+        // 로그인 버튼
         kakao_Btn.setOnClickListener {
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-                UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
-
-
+            if (userNickName_edit.getText().toString().trim()
+                    .equals("") || userClassNum_edit.getText().toString().trim().equals("")
+            ) {
+                Toast.makeText(this, "유저 정보를 입력하세요", Toast.LENGTH_SHORT).show()
             } else {
-                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                    UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
+
+                } else {
+                    UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                }
             }
-
-
         }
     }
 
@@ -123,11 +132,12 @@ class LoginActivity : AppCompatActivity() {
                 DBHelper.profile_Src = user.kakaoAccount?.profile?.thumbnailImageUrl
 
                 //파이어 베이스에 id 마다 데이터 할당
-                DBHelper.database.child("User").child("UID_${DBHelper.id}").child("userID").setValue(DBHelper.id)
+                DBHelper.database.child("User").child("UID_${DBHelper.id}").child("userID")
+                    .setValue(DBHelper.id)
                 UserDataSet()
                 //여기까지지
             }
-       }
+        }
     }
 
     private fun UserInit() {
@@ -147,7 +157,7 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    private fun UserDataSet(){
+    private fun UserDataSet() {
 
         //닉네임
         this.userNickname = userNickName_edit.text.toString()
@@ -157,9 +167,43 @@ class LoginActivity : AppCompatActivity() {
         this.userClassNum = userClassNum_edit.text.toString()
 
         //>>>>>>>>>>>>>>>>>>
-        Log.e("로그인 액티비티","학번 ${userClassNum},  학과 ${userClass} , 닉네임 ${userNickname}")
+        Log.e("로그인 액티비티", "학번 ${userClassNum},  학과 ${userClass} , 닉네임 ${userNickname}")
 
         //DB
-        DBHelper.sendUserData(userNickname,userClass,userClassNum)
+        DBHelper.sendUserData(userNickname, userClass, userClassNum)
+    }
+
+    private fun autoLogin() {
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (error != null) {
+                Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
+            } else if (tokenInfo != null) {
+
+                Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                UserApiClient.instance.me { user, error ->
+                    if (error != null) {
+                        Log.e("kakaoTest", "사용자 정보 요청 실패", error)
+                    } else if (user != null) {
+                        Log.i(
+                            "kakaoTest", "사용자 정보 요청 성공" +
+                                    "\n회원번호: ${user.id}" +
+                                    "\n이메일: ${user.kakaoAccount?.email}" +
+                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
+                        )
+                        DBHelper.id = user.id
+                        Log.d("id값 1", "${DBHelper.id}")
+                        DBHelper.email = user.kakaoAccount?.email
+                        DBHelper.name = user.kakaoAccount?.profile?.nickname
+                        DBHelper.profile_Src = user.kakaoAccount?.profile?.thumbnailImageUrl
+
+                        DBHelper.getSurveyList()
+                        finish()
+                    }
+                }
+            }
+        }
     }
 }
